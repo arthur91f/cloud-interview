319135c2e25f3b7a251b9cd1e31c9f08fe3b50af
arthur91f <arthur91f@gmail.com>
2021-10-20 22:54:38 +0200

    ft(minikube host): 2nd installation step

--- host/2-pop_environment.sh
+++ host/2-pop_environment.sh
@@ -5,27 +5,47 @@ minikube start --driver=docker && \
 minikube status && \
 echo "minikube started" || (echo "minikube failed to start" ; exit 1 )
 
-## ADD TRAEFIK AS INGRESS CONTROLLER ------------------------------------------
-helm repo add traefik https://helm.traefik.io/traefik && \
-helm repo update && \
-helm install traefik traefik/traefik && \
-echo "helm started" || (echo "minikube failed to start" ; exit 1 )
-
 ## GET APPS SOURCE CODE -------------------------------------------------------
-git clone git@github.com:arthur91f/cloud-interview.git && \
+git clone https://github.com/arthur91f/cloud-interview.git && \
 git checkout proposition && \
 echo "sources getted" || (echo "fail to get sources" ; exit 1 )
 
-## BUILD APPS -----------------------------------------------------------------
-cd "$current_path/cloud-interview/apps/hello" && \
-docker build -t ornikar-hello . && \
-echo "hello image builded" || \
-(echo "fail to build hello image" ; cd "$current_path" ; exit 1 )
+## ADD TRAEFIK AS INGRESS CONTROLLER ------------------------------------------
+helm repo add traefik https://helm.traefik.io/traefik &&
+helm repo update &&
+cd cloud-interview/host &&
+helm install -f helm_values/traefik.yml traefik traefik/traefik &&
+echo "helm started" || (echo "traefik failed to install" ; exit 1 )
 
-cd "$current_path/cloud-interview/apps/world" && \
-docker build -t ornikar-world . && \
-echo "world image builded" || 
-(echo "fail to build world image" ; cd "$current_path" ; exit 1 )
+## DEPLOY APP  ----------------------------------------------------------------
+function install_app {
+  app="$1"
+  cd "$current_path/cloud-interview/apps/$app" && 
+  docker build -t ornikar-$app . && 
+  echo "$app image builded" || 
+  (echo "fail to build $app image" ; cd "$current_path" ; exit 1 )
 
-## DEPLOY APPS ----------------------------------------------------------------
+  minikube image load ornikar-$app:latest && 
+    echo "$app image loaded in minikube" || 
+    (echo "fail to load $app image in minikube" ; exit 1 )
 
+  cd "$current_path/cloud-interview/host"
+  helm install -f helm_values/ornikar-$app.yml ornikar-$app charts/front &&
+    echo "front $app deployed" || 
+    (echo "fail to deploy $app" ; cd "$current_path" ; exit 1)
+  
+  cd "$current_path"
+}
+
+for app in hello world ; do
+  install_app $app
+  echo "----------"
+done
+
+# helm upgrade -f helm_values/traefik.yml traefik traefik/traefik
+# helm upgrade -f helm_values/ornikar-hello.yml ornikar-hello charts/front
+# helm upgrade -f helm_values/ornikar-world.yml ornikar-world charts/front
+
+# helm uninstall traefik
+# helm uninstall ornikar-hello
+# helm uninstall ornikar-world
